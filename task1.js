c4ceb4e6cd4996d6a5a99ef5985964b6003cddf7
Mariam433477 <https://github.com/Mariam433477>
2022-03-06 02:30:57 +0200

    create project

---
+++ task1.js
@@ -0,0 +1,5 @@
+let x=500;
+let y=200;
+console.log(`the value of x before swapping ${x} the value of y before swapping ${y}`);
+[x,y]=[y,x];
+console.log(`the value of x after swapping ${x} the value of y after swapping ${y}`);
